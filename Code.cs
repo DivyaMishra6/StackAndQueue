616da8954ea7e9f6d8090d5915db86981895a097
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-20 11:19:21 +0530

    [Divya]:  Create Queue and add element in Queue

--- Code.cs
+++ Code.cs
@@ -9,13 +9,10 @@ namespace StackAndQueue
 {
     public class Code
     {
-        public Node top;
-        public void StackWithLinkedList()
-        {
-            this.top = null;
-        }
+        public Node top = null;
+        
 
-        public void push(int value)
+        public void Enqueue(int value)
         {
             Node node = new Node(value);
             if (this.top == null)
@@ -27,50 +24,23 @@ namespace StackAndQueue
                 node.next = this.top;
             }
             this.top = node;
-            Console.WriteLine("{0} pushed to stack", value);
+            Console.WriteLine("{0} Add element in Queue", value);
         }
 
 
         public void Display()
         {
             Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+
             while (temp != null)
             {
                 Console.WriteLine(temp.data + " ");
                 temp = temp.next;
-            }
-        }
-
-        //Peek top most element
-        public void Peek()
-        {
-            if (this.top == null)
-            {
-                Console.WriteLine("Stack is empty");
-                return;
-            }
-            Console.WriteLine($"{this.top.data} is the top of the stack ");
-        }
-
-        //Pop remove element
-        public void Pop()
-        {
-            if (this.top == null)
-            {
-                Console.WriteLine("Stack is empty,Deletion is not possible");
-                return;
-            }
-            Console.WriteLine($"Value poped is {this.top.data}");
-            this.top = this.top.next;
-        }
-
-        // Check Empty
-        public void IsEmpty()
-        {
-            while (this.top != null)
-            {
-                Peek();
-                Pop();
             }
         }
 
