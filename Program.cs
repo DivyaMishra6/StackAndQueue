a187d1d2696437683a0eb7efd1044a3ec57e583f
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-20 10:16:47 +0530

    [Divya]: Welcome to Stack And Queue Program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack And Queue Program!");
+            Console.WriteLine("#####################");
+            Code obj = new Code();
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            obj.Display();
+
+        }
+
+    }
+}
