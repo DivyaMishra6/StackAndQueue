76d2e419c362b5b6d0c85f5d03200f7adcb7e164
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-20 11:27:09 +0530

    [Divya]: Delete element in the Queue

--- Program.cs
+++ Program.cs
@@ -13,7 +13,11 @@ namespace StackAndQueue
             obj.Enqueue(30);
             obj.Enqueue(56);
 
-   
+            Console.WriteLine("Element stored in queue is:-");
+            obj.Display();
+            Console.WriteLine("After Dequeue operation:");
+            obj.Dequeue();
+            Console.WriteLine("Element stored in queue is:-");
             obj.Display();
 
         }
